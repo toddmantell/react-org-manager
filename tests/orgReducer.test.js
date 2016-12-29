0f3ef2028203f616d0ccf851af86cec365c6d244
todder02 <tmantell99@yahoo.com>
2016-12-28 21:43:45 -0600

    Moving actions to arrange in each unit test (orgsReducer)

--- tests/orgReducer.test.js
+++ tests/orgReducer.test.js
@@ -4,13 +4,12 @@ import * as actionTypes from '../actions/actionTypes';
 
 
 describe('Orgs Reducer Tests', () => {
-    const action = {
+    it('should return 2 orgs', () => {        
+        const action = {
             type: actionTypes.LOAD_ORGS,
             orgs: [{"orgId": 1, "orgFullName": "Denver1 Health Organization", "orgShortName": "Denver Health", "isActive": true},
                     {"orgId": 2, "orgFullName": "Great State Healthsystem", "orgShortName": "Great State", "isActive": false}]
         }
-
-    it('should return 2 orgs', () => {        
 
         let result = orgsReducer([], action);
 
@@ -18,21 +17,33 @@ describe('Orgs Reducer Tests', () => {
     });
 
     it('should find the 1st orgs fullName', () => {
+        const action = {
+            type: actionTypes.LOAD_ORGS,
+            orgs: [{"orgId": 1, "orgFullName": "Denver1 Health Organization", "orgShortName": "Denver Health", "isActive": true},
+                    {"orgId": 2, "orgFullName": "Great State Healthsystem", "orgShortName": "Great State", "isActive": false}]
+        }
+
         let result = orgsReducer([], action);
 
         expect(result[0].orgFullName).toEqual("Denver1 Health Organization");
     });
 
     it('should add a new org', () => {
+        const orgs = [
+                    {"orgId": 1, "orgFullName": "Denver1 Health Organization", "orgShortName": "Denver Health", "isActive": true},
+                    {"orgId": 2, "orgFullName": "Great State Healthsystem", "orgShortName": "Great State", "isActive": false}
+                    ];        
+
         const newOrg = {"orgId": 3, "orgFullName": "New Health Organization", "orgShortName": "New Health", "isActive": true};
 
-        const localAction = {
+        const action = {
             type: actionTypes.ADD_ORG,
             newOrg
         }
 
-        let result = orgsReducer(action.orgs, localAction);
+        let result = orgsReducer(orgs, action);
         
         expect(result[2].orgShortName).toEqual("New Health");
+        expect(result.length).toBe(3);
     });
 });
